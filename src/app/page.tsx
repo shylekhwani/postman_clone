045af672b5d9252b128afe61497a7339f6399223
Yash Lekhwani <yashlekhwani49@gmail.com>
2025-10-07 20:57:30 +0530

    Added Authentication using betterAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/UserButton";
 
-export default function Home() {
+export default async function Home() {
+  const currUser = await currentUser();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Button>Hello</Button>
+      <UserButton user={currUser} />
     </div>
   );
 }
